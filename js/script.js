fda2064216158153d3f8fbbff719a5b39db4dee5
Andrew Merrell and Stephen Newkirk <andrew+stephen@>
2016-08-09 11:55:08 -0700

    Makes all of grid layout and content.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $("#cardtemplate").click(function() {
-    $("#cardtemplate > p").toggle();
+  $("#card1").click(function() {
+    $("#card1 > p").toggle();
   });
 
   $("#card2").click(function() {
@@ -9,6 +9,66 @@ $(document).ready(function() {
 
   $("#card3").click(function() {
     $("#card3 > p").toggle();
+  });
+
+  $("#card4").click(function() {
+    $("#card4 > p").toggle();
+  });
+
+  $("#card5").click(function() {
+    $("#card5 > p").toggle();
+  });
+
+  $("#card6").click(function() {
+    $("#card6 > p").toggle();
+  });
+
+  $("#card7").click(function() {
+    $("#card7 > p").toggle();
+  });
+
+  $("#card8").click(function() {
+    $("#card8 > p").toggle();
+  });
+
+  $("#card9").click(function() {
+    $("#card9 > p").toggle();
+  });
+
+  $("#card10").click(function() {
+    $("#card10 > p").toggle();
+  });
+
+  $("#card11").click(function() {
+    $("#card11 > p").toggle();
+  });
+
+  $("#card12").click(function() {
+    $("#card12 > p").toggle();
+  });
+
+  $("#card13").click(function() {
+    $("#card13 > p").toggle();
+  });
+
+  $("#card14").click(function() {
+    $("#card14 > p").toggle();
+  });
+
+  $("#card15").click(function() {
+    $("#card15 > p").toggle();
+  });
+
+  $("#card16").click(function() {
+    $("#card16 > p").toggle();
+  });
+
+  $("#card17").click(function() {
+    $("#card17 > p").toggle();
+  });
+
+  $("#card18").click(function() {
+    $("#card18 > p").toggle();
   });
 
 });
