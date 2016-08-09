0ec2ed9a9dca0f1dd5136724aae9349aa7275bf5
Andrew Merrell and Stephen Newkirk <andrew@>
2016-08-09 11:21:36 -0700

    add grid template with js to turn paragraphs on and off.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#cardtemplate").click(function() {
+    $("#cardtemplate > p").toggle();
+  });
+
+  $("#card2").click(function() {
+    $("#card2 > p").toggle();
+  });
+
+  $("#card3").click(function() {
+    $("#card3 > p").toggle();
+  });
+
+});
