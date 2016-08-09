e8d990cce29b2ae5e0257ebfdd02430102ef7300
Andrew Merrell and Stephen Newkirk <stephen@>
2016-08-09 14:17:00 -0700

    Played with jQuery to change hover response and simplify code.

--- js/script.js
+++ js/script.js
@@ -1,74 +1,107 @@
 $(document).ready(function() {
+
+  // ============Work=in=Progress==============
+  // $(".col-md-3").hover(function() {
+  //   $(this).toggleClass("hoveron");
+  // });
+
+  // $(".col-md-3").click(function() {
+  //   $("p").children.fadeToggle();
+  // });
+
+  // $(".col-md-3 > p").click(function() {
+  //     $(this).fadeToggle();
+  // });
+  // ========================================
+  
   $("#card1").click(function() {
-    $("#card1 > p").toggle();
+    $("#card1 > p").fadeToggle();
+    $("#card1").toggleClass("selected");
   });
 
   $("#card2").click(function() {
-    $("#card2 > p").toggle();
+    $("#card2 > p").fadeToggle();
+    $("#card2").toggleClass("selected");
   });
 
   $("#card3").click(function() {
-    $("#card3 > p").toggle();
+    $("#card3 > p").fadeToggle();
+    $("#card3").toggleClass("selected");
   });
 
   $("#card4").click(function() {
-    $("#card4 > p").toggle();
+    $("#card4 > p").fadeToggle();
+    $("#card4").toggleClass("selected");
   });
 
   $("#card5").click(function() {
-    $("#card5 > p").toggle();
+    $("#card5 > p").fadeToggle();
+    $("#card5").toggleClass("selected");
   });
 
   $("#card6").click(function() {
-    $("#card6 > p").toggle();
+    $("#card6 > p").fadeToggle();
+    $("#card6").toggleClass("selected");
   });
 
   $("#card7").click(function() {
-    $("#card7 > p").toggle();
+    $("#card7 > p").fadeToggle();
+    $("#card7").toggleClass("selected");
   });
 
   $("#card8").click(function() {
-    $("#card8 > p").toggle();
+    $("#card8 > p").fadeToggle();
+    $("#card8").toggleClass("selected");
   });
 
   $("#card9").click(function() {
-    $("#card9 > p").toggle();
+    $("#card9 > p").fadeToggle();
+    $("#card9").toggleClass("selected");
   });
 
   $("#card10").click(function() {
-    $("#card10 > p").toggle();
+    $("#card10 > p").fadeToggle();
+    $("#card10").toggleClass("selected");
   });
 
   $("#card11").click(function() {
-    $("#card11 > p").toggle();
+    $("#card11 > p").fadeToggle();
+    $("#card11").toggleClass("selected");
   });
 
   $("#card12").click(function() {
-    $("#card12 > p").toggle();
+    $("#card12 > p").fadeToggle();
+    $("#card12").toggleClass("selected");
   });
 
   $("#card13").click(function() {
-    $("#card13 > p").toggle();
+    $("#card13 > p").fadeToggle();
+    $("#card13").toggleClass("selected");
   });
 
   $("#card14").click(function() {
-    $("#card14 > p").toggle();
+    $("#card14 > p").fadeToggle();
+    $("#card14").toggleClass("selected");
   });
 
   $("#card15").click(function() {
-    $("#card15 > p").toggle();
+    $("#card15 > p").fadeToggle();
+    $("#card15").toggleClass("selected");
   });
 
   $("#card16").click(function() {
-    $("#card16 > p").toggle();
+    $("#card16 > p").fadeToggle();
+    $("#card16").toggleClass("selected");
   });
 
   $("#card17").click(function() {
-    $("#card17 > p").toggle();
+    $("#card17 > p").fadeToggle();
+    $("#card17").toggleClass("selected");
   });
 
   $("#card18").click(function() {
-    $("#card18 > p").toggle();
+    $("#card18 > p").fadeToggle();
+    $("#card18").toggleClass("selected");
   });
 
 });
